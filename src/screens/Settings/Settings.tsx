e2bc90f7838bda46dd4ed7247376ca96af38b43c
Ahmed Samir <ahmed.elhalwany@forus.cc>
2021-11-10 01:49:53 +0200

    add settings screen

---
+++ src/screens/Settings/Settings.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { CustomLayout, Section } from '@components/common';
+import styles from './styles';
+import { LANGUAGES_OPTIONS, THEMES_OPTIONS } from '@utils/constants';
+import {
+  changeLanguage,
+  getCurrentLanguage,
+  strings,
+} from '../../localization';
+import { IOption } from 'types';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectTheme, setTheme } from '@store/slices/themSlice';
+import { RadioGroup } from '@components/Form';
+
+export const SettingsScreen = () => {
+  const theme = useSelector(selectTheme);
+  const dispatch = useDispatch();
+  return (
+    <CustomLayout style={styles.layout}>
+      <Section title={strings('languages.title')}>
+        <RadioGroup
+          initialValue={LANGUAGES_OPTIONS.find(
+            value => value.value === getCurrentLanguage(),
+          )}
+          data={LANGUAGES_OPTIONS}
+          onSelect={(item: IOption) => {
+            changeLanguage(item.value);
+          }}
+        />
+      </Section>
+
+      <Section title={strings('themes.title')}>
+        <RadioGroup
+          initialValue={THEMES_OPTIONS.find(value => value.value === theme)}
+          data={THEMES_OPTIONS}
+          onSelect={(item: IOption) => {
+            dispatch(setTheme(item.value));
+          }}
+        />
+      </Section>
+    </CustomLayout>
+  );
+};

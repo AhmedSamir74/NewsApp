df94dfe511b3690038d716f12e750f526e0a2062
Ahmed Samir <ahmed.elhalwany@forus.cc>
2021-11-10 01:49:09 +0200

    add news service (API fetching)

---
+++ src/store/index.ts
@@ -0,0 +1,30 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import themReducer from './slices/themSlice';
+import { newsService } from '@services/newsService';
+
+export const store = configureStore({
+  reducer: {
+    theme: themReducer,
+    // Add the generated reducer as a specific top-level slice
+    [newsService.reducerPath]: newsService.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat([newsService.middleware]),
+});
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

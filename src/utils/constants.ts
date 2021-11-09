7815877a49cf4c1704898fd88c264b6e8c0c86e4
Ahmed Samir <ahmed.elhalwany@forus.cc>
2021-11-10 01:37:39 +0200

    add utils like formatter functions

---
+++ src/utils/constants.ts
@@ -0,0 +1,32 @@
+export const BASE_URL = 'https://newsapi.org/v2/';
+// export const API_KEY = 'e4da63cd651940d7b593f2fdf8092aaa' ;
+export const API_KEY = 'eba4faaab9804c089e6160c97f75fdc5';
+
+export const NEW_DETAILS_DYNAMIC_LINK = 'https://newsapplication.page.link/new';
+export const IS_NEW_USER_KEY = 'isNewUser';
+
+export const THEMES_OPTIONS = [
+  {
+    id: 1,
+    value: 'light',
+    title: 'themes.light',
+  },
+  {
+    id: 2,
+    value: 'dark',
+    title: 'themes.dark',
+  },
+];
+
+export const LANGUAGES_OPTIONS = [
+  {
+    id: 1,
+    value: 'en',
+    title: 'languages.en',
+  },
+  {
+    id: 2,
+    value: 'ar',
+    title: 'languages.ar',
+  },
+];
